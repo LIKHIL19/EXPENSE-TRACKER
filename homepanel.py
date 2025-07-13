d56dcb373d880706fec6fbd75175d98c9be236d9
Likhil Mopuri <154863174+LIKHIL19@users.noreply.github.com>
2025-07-13 21:23:50 +0530

    Add files via upload

---
+++ homepanel.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+root = tk.Tk()
+root.title("Home Panel")
+root.geometry("750x800")
+root.resizable(False,False)
+root.configure(bg="#1a2233")
+
+# Color Palette
+BG_WINDOW   = "#202B43"
+BG_HEADER   = "#232b3a"
+BG_INPUT    = "#f5f7fa"
+TEXT_ACCENT = "#aee938"
+
+
+
+
+root.mainloop()
